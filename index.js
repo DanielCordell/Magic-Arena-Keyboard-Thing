e7f5f3ee73a3c5f5b5a86d2dccbad6d324dc7170
Daniel Cordell <danielbc@hotmail.co.uk>
2019-07-12 01:27:21 +0100

    First steps

--- index.js
+++ index.js
@@ -1,15 +1,38 @@
 const path = require('path')
 const fs = require('fs');
+const request = require('request');
 
 const propsPath = path.join("C:", "ProgramData", "SteelSeries", "SteelSeries Engine 3", "coreProps.json");
 
-var url;
+var apiURL;
+
+const gameName = "MAGIC_ARENA";
+const gameDisplay = "Magic Arena Color";
+const developer = "Daniel Cordell"
 
 fs.readFile(propsPath, 'utf8', (err, file) => {
   if (err) {
     console.log(err);
     process.exit(-1);
   }
-  url = JSON.parse(file).address;
-  console.log(url);
+  apiURL = "http://" + JSON.parse(file).address;
+  console.log(apiURL);
+
+  setupGameData();
 })
+
+
+setupGameData = () => {
+  request.post({
+    headers: {'content-type' : 'application/json'},
+    url:     apiURL + "/game_metadata",
+    body:    JSON.stringify({
+      "game": gameName,
+      "game_display_name": gameDisplay,
+      "developer": developer
+    })
+  }, function(error, response, body){
+    console.log(error);
+    console.log(body);
+  });  
+}
